c3b348d1d7aa34002302a3620b1da5aaae50397c
Aayush <81736883+aayushg1007@users.noreply.github.com>
2023-06-15 23:53:02 +0530

    Update sol2.R

--- sol2.R
+++ sol2.R
@@ -16,9 +16,14 @@ flip = function(a) {
   
 }
 
-#c
+#d
+
 html= read_html("https://stats.stackexchange.com/questions?tab=Votes")
 title = html%>%html_elements(".s-post-summary--content-title a")%>%html_text()
-views = html%>%html_elements(".s-user-card--rep span")%>%html_text()
-ans = html%>%html_elements(".s-post-summary--stats-item-number")%>%html_text("span[1]")
+#views = html%>%html_elements(".s-user-card--rep span")%>%html_text()
+ans = html%>%html_elements(".s-post-summary--stats-item-number")%>%html_text()
+votes = ans[seq_along(ans) %%3 == 1]
+nans = ans[seq_along(ans) %%3 == 2]
+views = ans[seq_along(ans) %%3 == 0]
+data=data.frame(Title=title, Views=views,Answers=nans,Votes=votes)
 
